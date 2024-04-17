d3711b9bb5102298eb8ca89a41cabfc5af11066f
annia <afunesmrtinez@udesa.edu.ar>
2024-04-17 09:41:01 -0300

    cambios en el header

--- proyecto/src/components/Header/Header.js
+++ proyecto/src/components/Header/Header.js
@@ -11,9 +11,11 @@ export default function Header() {
             <div className="headera">
             <Link  to="/"><h1>Home</h1></Link>
             <Link  to="/favoritos"><h1>Favoritos</h1></Link>
-            <Link  to="/VerTodoPelis"><h1>Películas</h1></Link>
+            <Link  to="/VerTodoPelis"><h1>Películas Populares</h1></Link>
+            <Link  to="/VerTodoRated"><h1>Películas Best Rated</h1></Link>
+
             </div>
         </nav>
        </header>
     )
-  }
+  }localStorage
